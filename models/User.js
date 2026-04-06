44c3c6ced32369aaaeb0bdf1d08058cc267a8ffd
Sunju17th <sunju17th@gmail.com>
2026-04-07 03:47:29 +0700

    cap nhat lai model User

--- models/User.js
+++ models/User.js
@@ -23,19 +23,7 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-// Middleware băm mật khẩu trước khi lưu
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) {
-        next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
 
-// Phương thức để kiểm tra mật khẩu (dùng cho controller đăng nhập)
-userSchema.methods.matchPassword = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const User = mongoose.model('User', userSchema);
 export default User;
